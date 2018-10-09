95ff681360da3459f2ff7f3e931fcb7fdde15501
Carson Brownlee <carson.brownlee@intel.com>
2018-10-09 15:50:55 -0400

    removing unecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carson/git/hdOSPRayPlugin
-# Build directory: /home/carson/git/hdOSPRayPlugin/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("pxr/imaging/plugin/hdOSPRay")
--- build/pxr/imaging/plugin/hdOSPRay/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carson/git/hdOSPRayPlugin/pxr/imaging/plugin/hdOSPRay
-# Build directory: /home/carson/git/hdOSPRayPlugin/build/pxr/imaging/plugin/hdOSPRay
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
