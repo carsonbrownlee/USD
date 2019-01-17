5f0cbd2ab5c15d9d29f6414fb2354ba0e154a15a
Bruce Cherniak <bruce.cherniak@intel.com>
2019-01-17 11:06:27 -0600

    Move load module ptex after other ospInit

--- pxr/imaging/plugin/hdOSPRay/renderDelegate.cpp
+++ pxr/imaging/plugin/hdOSPRay/renderDelegate.cpp
@@ -87,7 +87,6 @@ HdOSPRayRenderDelegate::HdOSPRayRenderDelegate()
     av[i] = args[i - 1].c_str();
   }
   try {
-      ospLoadModule("ptex"); // Required for ptex textures
       int init_error = ospInit(&ac,av);
       if (init_error != OSP_NO_ERROR) {
         std::cerr << "FATAL ERROR DURING INITIALIZATION!" << std::endl;
@@ -116,6 +115,9 @@ HdOSPRayRenderDelegate::HdOSPRayRenderDelegate()
     ospInit(&ac, av);
   }
   delete [] av;
+
+  ospLoadModule("ptex"); // Required for ptex textures
+
   std::cout << "finished ospray init" << std::endl;
 
   _model = ospNewModel();
